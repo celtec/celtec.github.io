3869ff7eda128106ae2a93aefb96c654edb20279
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2016-11-07 14:33:33 -0200

    Set SITEURL for publish conf

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'https://celtec.github.io/jobs'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
